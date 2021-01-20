60d3713e8603a492cda0083be8a6c80d3c39a6a4
JeongHyun <k557330@naver.com>
2021-01-20 13:58:09 +0900

    head 요소 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import "../style.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <>
+            <Head>
+                <title>asd</title>
+            </Head>
             <Component {...pageProps} />
         </>
     );
