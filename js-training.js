b2fe7e73610cb1541c239c03e2dc2126276c8fcb
JeongHyun <k557330@naver.com>
2021-02-19 19:50:09 +0900

    setTimeout setInterval

--- js-training.js
+++ js-training.js
@@ -1,5 +1,15 @@
-console.time("answer_time");
+const timer = setTimeout(() => {
+    console.log("3s wait");
 
-alert("confirm");
+    const interval = setInterval(() => {
+        console.log("3s");
+    }, 3000);
 
-console.timeEnd("answer_time");
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 12000);
+}, 3000);
+
+//3초 기다리고 나서 3초마다 3s 찍는 중에 12초가 되면 setInterval 취소
+
+//clearTimeout(timer) setTimeout 취소
