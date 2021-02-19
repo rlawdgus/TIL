2e932b2a884f91d251e55b8c7998fc34958ff728
JeongHyun <k557330@naver.com>
2021-02-19 20:22:17 +0900

    function arguments

--- js-training.js
+++ js-training.js
@@ -1,15 +1,9 @@
-const timer = setTimeout(() => {
-    console.log("3s wait");
+function func(a, b, c) {
+    console.log(arguments)
+    console.log(arguments.length)
+    console.log(arguments.callee)
+}
 
-    const interval = setInterval(() => {
-        console.log("3s");
-    }, 3000);
+func("a", 2, "3", 4, 5)
 
-    setTimeout(() => {
-        clearInterval(interval);
-    }, 12000);
-}, 3000);
-
-//3초 기다리고 나서 3초마다 3s 찍는 중에 12초가 되면 setInterval 취소
-
-//clearTimeout(timer) setTimeout 취소
+// 화살표 함수는 arguments가 없음
