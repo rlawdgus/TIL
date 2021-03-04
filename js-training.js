abf09cc853a1ab82556045a621b5b26012bd0e21
jeonghyun <k557330@naver.com>
2021-03-04 22:46:21 +0900

    jsonp.js

--- js-training.js
+++ js-training.js
@@ -1,20 +1,11 @@
+const show = (data) => {
+    console.log(data.name);
+    console.log(data.price);
+};
+
 window.onload = () => {
-    let req = new XMLHttpRequest();
-
-    let jsonObj;
-    req.addEventListener(
-        "load",
-        () => {
-            jsonObj = req.response;
-            console.log(jsonObj);
-        },
-        false
-    );
-
-    req.responseType = "json";
-
-    req.open("GET", "data.json", true);
-    req.send(null);
-
-    console.log(req);
+    let url = "jsonp.js";
+    let script = document.createElement("script");
+    script.setAttribute("src", url);
+    document.getElementsByTagName("head")[0].appendChild(script);
 };
