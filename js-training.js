2270057a4c4a64ecd5bcea48821d284a3cdf4d39
jeonghyun <k557330@naver.com>
2021-03-05 22:20:11 +0900

    file reader image upload

--- js-training.js
+++ js-training.js
@@ -1,3 +1,44 @@
-const a1 = document.getElementById("a1");
-const blob1 = new Blob(["Hello ", "download!"], { type: "text/plain" });
-a1.href = URL.createObjectURL(blob1);
+window.onload = () => {
+    const input = document.getElementById("input");
+
+    input.addEventListener(
+        "change",
+        (e) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+                const img = new Image();
+                img.onload = () => {
+                    const canvas = document.createElement("canvas");
+                    const context = canvas.getContext("2d");
+                    context.drawImage(img, 0, 0);
+
+                    //back & white
+                    // const imageData = context.getImageData(
+                    //     0,
+                    //     0,
+                    //     canvas.width,
+                    //     canvas.height
+                    // );
+                    // const data = imageData.data;
+
+                    // for (let i = 0; i <= data.length; i += 4) {
+                    //     const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+                    //     data[i] = avg;
+                    //     data[i + 1] = avg;
+                    //     data[i + 2] = avg;
+                    // }
+
+                    // context.putImageData(imageData, 0, 0);
+
+                    document.body.appendChild(img);
+                };
+
+                img.src = reader.result;
+            };
+
+            // reader.readAsText(input.files[0]);
+            reader.readAsDataURL(input.files[0]);
+        },
+        false
+    );
+};
