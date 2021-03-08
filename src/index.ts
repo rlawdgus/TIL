38f545494e0309376b5a050cb4258346e60e4a99
jeonghyun <k557330@naver.com>
2021-03-08 19:43:15 +0900

    generic type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-import { testMakePerson } from "./utils/makePerson";
+class Container<T> {
+    constructor(public vlaue: T) {}
+}
 
-testMakePerson();
+let numberContainer: Container<number> = new Container<number>(1);
+let stringContainer: Container<string> = new Container<string>("Hello world !");
--- src/utils/makePerson.ts
+++
@@ -1,7 +0,0 @@
-export const makePerson = (name: string, age: number) => {
-    return { name: name, age: age };
-};
-
-export const testMakePerson = () => {
-    console.log(makePerson("Jane", 22), makePerson("Jack", 23));
-};
