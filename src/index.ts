0e12ab04ae67a6fc5dfa86e1018847cfd05053c6
jeonghyun <k557330@naver.com>
2021-03-11 16:19:10 +0900

    closure

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,14 @@
-type FirstOrderFunc<T, R> = (T: T) => R;
-type SecondOrderFunc<T, R> = (T: T) => FirstOrderFunc<T, R>;
-type ThirdOrderFunc<T, R> = (T: T) => SecondOrderFunc<T, R>;
+const makeNames = (): (() => string) => {
+    const names = ["A", "B", "C"];
+    let index = 0;
 
-const inc: FirstOrderFunc<number, number> = (x: number): number => x + 1;
+    return (): string => {
+        if (index === names.length) index = 0;
 
-const add: SecondOrderFunc<number, number> = (
-    x: number
-): FirstOrderFunc<number, number> => (y: number) => x + y;
+        return names[index++];
+    };
+};
 
-const add3: ThirdOrderFunc<number, number> = (
-    x: number
-): SecondOrderFunc<number, number> => (
-    y: number
-): FirstOrderFunc<number, number> => (z: number): number => x + y + z;
+const makeName: () => string = makeNames();
 
-console.log(inc(1));
-
-console.log(add(1)(2));
-
-console.log(add3(1)(2)(3));
-
-const add2: SecondOrderFunc<number, number> = add3(1);
-const add1: FirstOrderFunc<number, number> = add2(2);
-
-console.log(add1(3), add2(2)(3), add3(1)(2)(3));
+console.log([1, 2, 3, 4, 5, 6].map(() => makeName()));
