30bdca8b3c0b942abc912c846af17a0f570adad1
jeonghyun <k557330@naver.com>
2021-03-19 19:40:39 +0900

    initial data

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import fs from "fs";
 import path from "path";
+import * as url from "url";
 import { renderToString } from "react-dom/server";
 import React from "react";
 import App from "./App";
@@ -14,11 +15,18 @@ const html = fs.readFileSync(
 app.use("/dist", express.static("dist"));
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 app.get("*", (req, res) => {
-    const renderString = renderToString(<App initPage="home" />);
-    const result = html.replace(
-        '<div id="root"></div>',
-        `<div id="root">${renderString}</div>`
-    );
+    const parsedUrl = url.parse(req.url, true);
+    const page =
+        parsedUrl.pathname === "/" ? "home" : parsedUrl.pathname.substr(1);
+
+    const renderString = renderToString(<App initPage={page} />);
+    const initialData = { page };
+    const result = html
+        .replace(
+            '<div id="root"></div>',
+            `<div id="root">${renderString}</div>`
+        )
+        .replace("__DATA_FROM_SERVER__", JSON.stringify(initialData));
     res.send(result);
 });
 app.listen(3000);
